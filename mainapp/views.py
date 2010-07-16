b45a3dffdf5771123a39feadd6b617e007e00b14
Parker Phinney <parker.phinney@gmail.com>
2010-07-15 23:42:20 -0400

    when you submit a vote, you get a page that lists all of the votes so far

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,16 +4,18 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import real_time_voting.mainapp.models
+import datetime
 
 def main(request):
     return render_to_response('index.html', {})
 
 def vote_success(request):
-    return render_to_response('voting_success.html', {'successful_vote': True})
+    votes = real_time_voting.mainapp.models.Vote.objects.order_by('timestamp').filter(timestamp__isnull=False).reverse()
+    return render_to_response('voting_success.html', {'successful_vote': True, 'votes': votes})
 
 def process_vote_do(request):
     weight = request.POST['weight']
-    newvote = real_time_voting.mainapp.models.Vote(weight=weight)
+    newvote = real_time_voting.mainapp.models.Vote(weight=weight, timestamp=datetime.datetime.now(), ip_address=request.META.get('REMOTE_ADDR'))
     newvote.save()
 
     url_to_redirect_to = reverse(real_time_voting.mainapp.views.vote_success)
