28063da7d807e83743d4dc70c8275ad817556936
Parker Phinney <parker.phinney@gmail.com>
2011-03-05 19:50:41 -0500

    votes show up in real time now--but the method is inefficient

--- static/jsincludes.js
+++ static/jsincludes.js
@@ -5,3 +5,10 @@ function jack_this_form_id_with_this_function(id, func){
             return false;
     });
 }
+
+function convert_seconds_since_vid_start_into_date(seconds){
+    // we offset the timestamp by this amount so that it starts at midnight and we get nicer timestamps
+    offset = units_array['hours']*5;
+
+    return new Date(seconds*1000+offset);
+}
